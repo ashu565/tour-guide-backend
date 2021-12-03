115294284b9877fe9e001f105da78362802d91f6
ashu565 <ashu9993063472@gmail.com>
2021-12-03 21:44:20 +0530

    fad

--- Models/Location.js
+++ Models/Location.js
@@ -1,45 +1,32 @@
 const mongoose = require('mongoose')
-const {ObjectId} = mongoose.Schema.Types
+const { ObjectId } = mongoose.Schema.Types
 const LocationSchema = new mongoose.Schema({
-    location:{
-      type:String,
-      required:true
-    },
-    guide:{
-      type:ObjectId,
-<<<<<<< HEAD
-      ref:"Guide"
-    },
-    Hotel:{
-      type:ObjectId,
-      ref:"Hotel"
-=======
-      ref:Guide
-    },
-    Hotel:{
-      type:ObjectId,
-      ref:Hotel
->>>>>>> b5787c885bbf036ab8a0f204ba17ddeed3135ede
-    },
-    serviceType:{
-      type:String,
-      required:true,
-    },
-    service:{
-      type:ObjectId,
-<<<<<<< HEAD
-      refPath:"serviceType"
-    },
-=======
-      refPath:serviceType
-    }
->>>>>>> b5787c885bbf036ab8a0f204ba17ddeed3135ede
-    Amount:{
-      type:Double,
-      required:true
-    }
-},)
+  location: {
+    type: String,
+    required: true
+  },
+  guide: {
+    type: ObjectId,
+    ref: Guide
+  },
+  Hotel: {
+    type: ObjectId,
+    ref: Hotel
+  },
+  serviceType: {
+    type: String,
+    required: true,
+  },
+  service: {
+    type: ObjectId,
+    refPath: serviceType
+  },
+  Amount: {
+    type: Double,
+    required: true
+  }
+})
 
-const Location = mongoose.model("Location",locationSchema)
+const Location = mongoose.model("Location", locationSchema)
 
 module.exports = Location
--- Models/User.js
+++ Models/User.js
@@ -1,49 +1,45 @@
 const mongoose = require('mongoose')
-const {ObjectId} = mongoose.Schema.Types
-const validator=require('validator');
+const { ObjectId } = mongoose.Schema.Types
+const validator = require('validator');
 const userSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
-        index:true
+    firstName: {
+        type: String,
+        required: true,
+        index: true
     },
-    lastName:{
-        type:String,
-        required:true,
-        index:true
+    lastName: {
+        type: String,
+        required: true,
+        index: true
     },
-    contact:{
-      type:String,
-      required:true
+    contact: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        validate:[validator.isEmail,'Please provide a valid email']
+    email: {
+        type: String,
+        required: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    confirmPassword:{
-        type:String,
-        required:true
+    confirmPassword: {
+        type: String,
+        required: true
     },
-    role:{
-      type:String,
-      enum:['Traveller','Guide','Hotel','Admin'],
-      default:'Traveller',
-      required:true
+    role: {
+        type: String,
+        enum: ['Traveller', 'Guide', 'Hotel', 'Admin'],
+        default: 'Traveller',
+        required: true
     },
-    specifics:{
-      type:ObjectId,
-<<<<<<< HEAD
-      refPath:"role"
-=======
-      refPath:"role"
->>>>>>> b5787c885bbf036ab8a0f204ba17ddeed3135ede
+    specifics: {
+        type: ObjectId,
+        refPath: "role"
     }
 })
 
-const User = mongoose.model("User",userSchema)
+const User = mongoose.model("User", userSchema)
 module.exports = User
