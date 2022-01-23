299c026995f7c3d6c2ae0d605fcb12d3d398c378
arhamathar <arhamathar789@gmail.com>
2022-01-23 21:23:25 +0530

    booking routes added

--- controller/hotelController.js
+++ controller/hotelController.js
@@ -1,199 +1,207 @@
-const User = require("../Models/User");
-const AppError = require("../utils/appError");
-const Hotel = require("../Models/Hotel");
-const Booking = require("../Models/Booking");
-const Review = require("../Models/review");
-const APIFeatures = require("./../utils/apiFeatures");
+const User = require('../Models/User');
+const AppError = require('../utils/appError');
+const Hotel = require('../Models/Hotel');
+const Booking = require('../Models/Booking');
+const Review = require('../Models/review');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.getHotelsWithin = async (req, res, next) => {
-  try {
-    const { distance, latlng } = req.params;
-    const [lat, lng] = latlng.split(",");
+    try {
+        const { distance, latlng } = req.params;
+        const [lat, lng] = latlng.split(',');
 
-    const radius = distance / 3963.2; //To convert in radian from KM
+        const radius = distance / 3963.2; //To convert in radian from KM
 
-    const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
 
-    if (!lat || !lng) {
-      next(
-        new AppError(
-          "Please provide latitutr and longitude in the format lat,lng.",
-          400
-        )
-      );
+        if (!lat || !lng) {
+            next(
+                new AppError(
+                    'Please provide latitutr and longitude in the format lat,lng.',
+                    400
+                )
+            );
+        }
+
+        console.log(lat, lng, page, limit);
+
+        const results = {};
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+
+        if (startIndex > 0) {
+            results.previous = {
+                page: page - 1,
+                limit: limit,
+            };
+        }
+
+        results.hotels = await Hotel.find({
+            locations: { $geoWithin: { $centerSphere: [[lat, lng], radius] } },
+        })
+            .limit(limit)
+            .skip(startIndex)
+            .exec();
+
+        results.next = {
+            page: page + 1,
+            limit: limit,
+        };
+
+        res.status(200).json({
+            status: 'success',
+            results: results.hotels.length,
+            data: {
+                data: results,
+            },
+        });
+    } catch (err) {
+        console.log(err);
+        return next(
+            new AppError(
+                'Something went wrong, Could not find Hotels within',
+                500
+            )
+        );
     }
-
-    console.log(lat, lng, page, limit);
-
-    const results = {};
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-
-    if (startIndex > 0) {
-      results.previous = {
-        page: page - 1,
-        limit: limit,
-      };
-    }
-
-    results.hotels = await Hotel.find({
-      locations: { $geoWithin: { $centerSphere: [[lat, lng], radius] } },
-    })
-      .limit(limit)
-      .skip(startIndex)
-      .exec();
-
-    results.next = {
-      page: page + 1,
-      limit: limit,
-    };
-
-    res.status(200).json({
-      status: "success",
-      results: results.hotels.length,
-      data: {
-        data: results,
-      },
-    });
-  } catch (err) {
-    console.log(err);
-    return next(
-      new AppError("Something went wrong, Could not find Hotels within", 500)
-    );
-  }
 };
 
 exports.getHotelDetails = async (req, res, next) => {
-  try {
-    const hotel = await Hotel.findById(req.params.id);
-    console.log(req.params.id);
-    res.status(200).json({
-      status: "Success",
-      data: hotel,
-    });
-  } catch (err) {
-    console.log(err);
-    return next(
-      new AppError("Something went wrong, Could not find Hotels Details", 500)
-    );
-  }
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        console.log(req.params.id);
+        res.status(200).json({
+            status: 'Success',
+            data: hotel,
+        });
+    } catch (err) {
+        console.log(err);
+        return next(
+            new AppError(
+                'Something went wrong, Could not find Hotels Details',
+                500
+            )
+        );
+    }
 };
 
 exports.getHotelCurrentBooking = async (req, res, next) => {
-  try {
-    const hotel = await Hotel.findById(req.params.id);
-    if (req.user.specifics.toString() !== hotel.hotelManager.toString()) {
-      return next(new AppError("This hotel doesnt belong to you", 500));
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        if (req.user.specifics.toString() !== hotel.hotelManager.toString()) {
+            return next(new AppError('This hotel doesnt belong to you', 500));
+        }
+        const bookings = await Booking.find({
+            hotel: req.params.id,
+            endingDate: { $gte: new Date() },
+        });
+        res.status(200).json({
+            status: 'Success',
+            data: bookings,
+        });
+    } catch (err) {
+        console.log(err);
+        return next(
+            new AppError(
+                'Something went wrong, Could not find Hotel Current Bookings',
+                500
+            )
+        );
     }
-    const bookings = await Booking.find({
-      hotel: req.params.id,
-      endingDate: { $gte: new Date() },
-    });
-    res.status(200).json({
-      status: "Success",
-      data: bookings,
-    });
-  } catch (err) {
-    console.log(err);
-    return next(
-      new AppError(
-        "Something went wrong, Could not find Hotel Current Bookings",
-        500
-      )
-    );
-  }
 };
 
 exports.getHotelPastBooking = async (req, res, next) => {
-  try {
-    const hotel = await Hotel.findById(req.params.id);
-    if (req.user.specifics.toString() !== hotel.hotelManager.toString()) {
-      return next(new AppError("This hotel doesnt belong to you", 500));
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        if (req.user.specifics.toString() !== hotel.hotelManager.toString()) {
+            return next(new AppError('This hotel doesnt belong to you', 500));
+        }
+        const bookings = await Booking.find({
+            hotel: req.params.id,
+            endingDate: { $lt: new Date() },
+        });
+        res.status(200).json({
+            status: 'Success',
+            data: bookings,
+        });
+    } catch (err) {
+        console.log(err);
+        return next(
+            new AppError(
+                'Something went wrong, Could not find Hotel Past Bookings',
+                500
+            )
+        );
     }
-    const bookings = await Booking.find({
-      hotel: req.params.id,
-      endingDate: { $lt: new Date() },
-    });
-    res.status(200).json({
-      status: "Success",
-      data: bookings,
-    });
-  } catch (err) {
-    console.log(err);
-    return next(
-      new AppError(
-        "Something went wrong, Could not find Hotel Past Bookings",
-        500
-      )
-    );
-  }
 };
 
 exports.getAllHotels = async (req, res, next) => {
-  try {
-    const features = new APIFeatures(Hotel.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
+    try {
+        const features = new APIFeatures(Hotel.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
 
-    const hotels = await features.query;
+        const hotels = await features.query;
 
-
-    // const hotels = await Hotel.find();
-    res.status(200).json({
-      status: "Success",
-      results:hotels.length,
-      data: hotels,
-    });
-  } catch (err) {
-    console.log(err);
-    return next(
-      new AppError("Something went wrong, Could not find Hotels", 500)
-    );
-  }
+        // const hotels = await Hotel.find();
+        res.status(200).json({
+            status: 'Success',
+            results: hotels.length,
+            data: hotels,
+        });
+    } catch (err) {
+        console.log(err);
+        return next(
+            new AppError('Something went wrong, Could not find Hotels', 500)
+        );
+    }
 };
 
 exports.getHotelReviews = async (req, res, next) => {
-  try {
-    const page = parseInt(req.query.page);
-    const limit = 4;
+    try {
+        const page = parseInt(req.query.page);
+        const limit = 4;
 
-    const results = {};
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
+        const results = {};
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
 
-    if (startIndex > 0) {
-      results.previous = {
-        page: page - 1,
-        limit: limit,
-      };
+        if (startIndex > 0) {
+            results.previous = {
+                page: page - 1,
+                limit: limit,
+            };
+        }
+
+        results.reviews = await Review.find({
+            hotel: req.params.id,
+        })
+            .limit(4)
+            .skip(startIndex)
+            .exec();
+
+        results.next = {
+            page: page + 1,
+            limit: limit,
+        };
+
+        res.status(200).json({
+            status: 'success',
+            results: results.reviews.length,
+            data: {
+                data: results,
+            },
+        });
+    } catch (err) {
+        console.log(err);
+        return next(
+            new AppError(
+                'Something went wrong, Could not find Hotel reviews',
+                500
+            )
+        );
     }
-
-    results.reviews = await Review.find({
-      hotel: req.params.id,
-    })
-      .limit(4)
-      .skip(startIndex)
-      .exec();
-
-    results.next = {
-      page: page + 1,
-      limit: limit,
-    };
-
-    res.status(200).json({
-      status: "success",
-      results: results.reviews.length,
-      data: {
-        data: results,
-      },
-    });
-  } catch (err) {
-    console.log(err);
-    return next(
-      new AppError("Something went wrong, Could not find Hotel reviews", 500)
-    );
-  }
 };
--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -4,12 +4,36 @@ const authController = require('../controller/authController');
 const hotelController = require('../controller/hotelController');
 
 router
-  .route('/hotels-within/:distance/center/:latlng')
-  .get(hotelController.getHotelsWithin);
+    .route('/hotels-within/:distance/center/:latlng')
+    .get(hotelController.getHotelsWithin);
 
-router.get('/getHotelDetails/:id', hotelController.getHotelDetails)
-router.get('/getHotelCurrentBooking/:id', authController.protect, hotelController.getHotelCurrentBooking)
-router.get('/getHotelPastBooking/:id', authController.protect, hotelController.getHotelPastBooking)
-router.get('/getAllHotels', hotelController.getAllHotels)
-router.get('/getHotelReviews/:id', hotelController.getHotelReviews)
+router.get('/getHotelDetails/:id', hotelController.getHotelDetails);
+router.get(
+    '/getHotelCurrentBooking/:id',
+    authController.protect,
+    hotelController.getHotelCurrentBooking
+);
+router.get(
+    '/getHotelPastBooking/:id',
+    authController.protect,
+    hotelController.getHotelPastBooking
+);
+router.get('/getAllHotels', hotelController.getAllHotels);
+router.get('/getHotelReviews/:id', hotelController.getHotelReviews);
+router.get(
+    '/getAllBooking',
+    authController.protect,
+    hotelController.getAllBooking
+);
+router.get(
+    '/getPastBooking',
+    authController.protect,
+    hotelController.getPastBooking
+);
+router.get(
+    '/getFutureBooking',
+    authController.protect,
+    hotelController.getFutureBooking
+);
+
 module.exports = router;
